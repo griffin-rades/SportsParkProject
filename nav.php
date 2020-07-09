7ec9a034cb099d33f6575a055e27afcf2bd50d6a
Griffin Rades <ggrade16@smumn.edu>
2020-07-09 10:45:48 -0500

    Changed to php to take advatage of file seperation

---
+++ nav.php
@@ -0,0 +1,21 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">
+        <img src="images/logo.png" width="100" height="75" alt="" loading="lazy">
+        </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+        <li class="nav-item active">
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="attractions.php">Attractions</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="pricing.php">Pricing</a>
+        </li>
+        </ul>
+    </div>
+</nav>
